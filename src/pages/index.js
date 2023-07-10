081e07e794ec7497c78f5e4c1ad2631fc5dc759a
rayyanekaputra <bullzhit99@gmail.com>
2023-07-10 21:29:27 +0800

    Added: Background image on HERO, Image Carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import '../pages/index.css'
 
 const Home = () => {
 return (
-	<div className='Hero'>
-		<div>
-			<img src='/logo192.png' alt='logo lab ubicon'></img>
+	<div>
+		<section className='hero'>
+			<div>
+				<img src='/logo192.png' alt='logo lab ubicon'></img>
+			</div>
+			<h1>Ubicon Lab</h1>
+			<h2>Ubiquitous Computing & Networking Lab.</h2>
+			<p>@HASANUDDIN UNIVERSITY </p>
+		</section>
+		<div className='carousel-running-container'>
+			<ul>
+				<li>
+					<div className='carousel-img'>
+
+					</div>
+				</li>
+				<li>
+					<div className='carousel-img'>
+
+					</div>
+				</li>
+				<li>
+					<div className='carousel-img'>
+
+					</div>
+				</li>
+				<li>
+					<div className='carousel-img'>
+
+					</div>
+				</li>
+				<li>
+					<div className='carousel-img'>
+
+					</div>
+				</li>
+				<li>
+					<div className='carousel-img'>
+
+					</div>
+				</li>
+			</ul>
 		</div>
-		<h1>Ubicon Lab</h1>
-		<h2>Ubiquitous Computing & Networking Lab.</h2>
-		<p>@HASANUDDIN UNIVERSITY </p>
 	</div>
+	
 );
 };
 
