634af54f147f4618ce5924a2ded1a384430191b0
rayyanekaputra <bullzhit99@gmail.com>
2023-07-10 16:04:43 +0800

    Added: Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages';
+import About from './pages/about';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React ADA
-        </a>
-      </header>
-    </div>
+    <Router>
+   
+
+      <Navbar/>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element={<About/>} />
+      </Routes>
+
+    </Router>
+
+    
   );
 }
 
---
+++ src/components/navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+return (
+	<div>
+        <NavLink to="/">
+            Home
+        </NavLink>
+        <NavLink to="/about">
+            About Us
+        </NavLink>
+    </div> 
+);
+};
+
+export default Navbar;
