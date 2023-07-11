037df34f744f2864403531fdb8a31f0bf0bbe61a
rayyanekaputra <bullzhit99@gmail.com>
2023-07-11 22:17:29 +0800

    style: navbar tanpa logo, flexboxed, rencana mau dikasih styled-components module
    asd
    exit

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import './index.css'
 const Navbar = () => {
 return (
-	<div style={{
-        backgroundColor: 'white'
-    }}>
+	<div className='navbar-container'>
         <NavLink to="/">
             Home
+        </NavLink>
+        <NavLink to="/blog">
+            Blog
         </NavLink>
         <NavLink to="/about">
             About Us
