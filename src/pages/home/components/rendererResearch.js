d79919cf2c7827fafa6c83ac2fd89f3f594d806f
rayyanekaputra <bullzhit99@gmail.com>
2023-07-14 22:05:20 +0800

    feat: tambahkan renderer si data research field:

---
+++ src/pages/home/components/rendererResearch.js
@@ -0,0 +1,19 @@
+import { research } from "../data/dataResearch";
+import "../components/rendererResearch.css";
+
+//CARI: apa bedanya export default vs export biasa?
+export function ListResearch() {
+  const rendererResearchList = research.map((researchField) => (
+
+    //jangan dikasih parent container, karena disetiap item bakalan punya kalau dikasih
+    //bukan secara keseluruhan
+
+    <li className="list-research-field-item" key={researchField.id}>
+      <p>{researchField.field}</p>
+    </li>
+  ));
+
+  //return dalam bentuk apaki
+  // return <ul>{rendererResearchList}</ul>; 
+  return rendererResearchList;
+}
