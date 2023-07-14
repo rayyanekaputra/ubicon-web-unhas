f5650f61a68240d24b94d43fe375ca996b27abe8
rayyanekaputra <bullzhit99@gmail.com>
2023-07-14 22:05:56 +0800

    feat: menambahkan section research field lab

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,112 +1,87 @@
-import React from 'react';
-import '../pages/index.css'
+import React from "react";
+import "../home/index.css";
+import { ListResearch } from "./components/rendererResearch";
 
 const Home = () => {
-return (
-	<div>
-		<section className='hero'>
-			<div className='logo-ubicon'>
-				<img src='/logo192.png' alt='logo lab ubicon'></img>
-			</div>
-			<h1>Ubicon Lab</h1>
-			<h2>Ubiquitous Computing & Networking Lab.</h2>
-			<p>@HASANUDDIN UNIVERSITY </p>
-		</section>
-		<div className='carousel-running-container'>
-			{/* infinite slider, duplikat supaya seamless */}
-			<ul>
-				<li>
-					<div className='carousel-img'>
+  return (
+    <div>
+      <section className="hero">
+        <div className="logo-ubicon">
+          <img src="/logo192.png" alt="logo lab ubicon"></img>
+        </div>
+        <h1>Ubicon Lab</h1>
+        <h2>Ubiquitous Computing & Networking Lab.</h2>
+        <p>@HASANUDDIN UNIVERSITY </p>
+      </section>
+      <div className="carousel-running-container">
+        {/* infinite slider, duplikat supaya seamless */}
+        <ul>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+        </ul>
 
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
+        <ul>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+        </ul>
 
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
+        <ul>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+          <li>
+            <div className="carousel-img"></div>
+          </li>
+        </ul>
+      </div>
+      <section className="research-type">
+        <h1>Connecting Devices with Our Research</h1>
+        <p className="p1">
+          Our lab have multiple concentration that surrounds internet
+          engineering
+        </p>
 
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				
-			</ul>
-
-			<ul>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				
-			</ul>
-
-			<ul>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				<li>
-					<div className='carousel-img'>
-
-					</div>
-				</li>
-				
-			</ul>
-			
-			
-		</div>
-		
-	</div>
-	
-);
+        <ul className="list-research-wrapper">
+          <ListResearch />
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
