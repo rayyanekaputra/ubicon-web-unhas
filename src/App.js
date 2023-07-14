19d34bd279521d5a1fc9493a1020d184b6860805
rayyanekaputra <bullzhit99@gmail.com>
2023-07-14 22:07:27 +0800

    fix: missing use logo.svg

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import Navbar from './components/navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './pages';
-import About from './pages/about';
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/index";
+import About from "./pages/about";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-      {/* https://reactrouter.com/en/main/components/routes */}
-        <Route path='/' element = {<Home/>}/>
-        <Route path='/about' element={<About/>} />
+        {/* https://reactrouter.com/en/main/components/routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
-
-    
   );
 }
 
