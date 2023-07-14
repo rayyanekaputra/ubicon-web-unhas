347b13be907f8428ecb79c0721fc8ac69e6b07fc
rayyanekaputra <bullzhit99@gmail.com>
2023-07-14 14:53:03 +0800

    feat: menambahkan logo pada navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './index.css'
+
+//penamaan import ikuti peraturan tsx
+import navbar_logo from '../navbar/assets/navbar-logo.png';
+
 const Navbar = () => {
 return (
 	<div className='navbar-container'>
-        <NavLink to="/">
-            Home
-        </NavLink>
-        {/* TODO: buatkan pagenya dan router di App.js */}
-        <NavLink to="/blog">
-            Blog
-        </NavLink>
-        <NavLink to="/about">
-            About Us
-        </NavLink>
-    </div> 
+        <div className='navbar-logo'>
+            {/* panggil import */}
+            <img src={navbar_logo} alt=''></img>
+        </div>
+        <div className='navbar-menu'>
+            <NavLink to="/">
+                Home
+            </NavLink>
+            {/* TODO: buatkan pagenya dan router di App.js */}
+            <NavLink to="/blog">
+                Blog
+            </NavLink>
+            <NavLink to="/about">
+                About Us
+            </NavLink>
+        </div> 
+        <div className='navbar-logo'>
+            {/* panggil import navbar logo */}
+            <img src={navbar_logo} alt='' style={{
+                display: 'none'
+            }}>
+
+            </img>
+        </div>
+    </div>
+        
 );
 };
 
