8a95820e6ece128ad411d5fbd820c1a7f346326c
rayyanekaputra <bullzhit99@gmail.com>
2023-07-14 22:03:50 +0800

    feat: tambahkan data supaya scalable

---
+++ src/pages/home/data/dataResearch.js
@@ -0,0 +1,39 @@
+
+export const research = [
+  {
+    id: 0, // Used in JSX as a key
+    field: "Cloud Computing",
+  },
+  {
+    id: 1, // Used in JSX as a key
+    field: "Edge Computing",
+  },
+  {
+    id: 2, // Used in JSX as a key
+    field: "Software Defined Network (SDN)",
+  },
+  {
+    id: 3, // Used in JSX as a key
+    field: "Embedded System",
+  },
+  {
+    id: 4, // Used in JSX as a key
+    field: "Web and Mobile Apps",
+  },
+  {
+    id: 5, // Used in JSX as a key
+    field: "Delay Tolerant Network (DTN)",
+  },
+  {
+    id: 6, // Used in JSX as a key
+    field: "Internet of Everything (IoE)",
+  },
+  {
+    id: 7, // Used in JSX as a key
+    field: "Internet for Developing (I4D)",
+  },
+  {
+    id: 8, // Used in JSX as a key
+    field: "Internet Security",
+  },
+];
